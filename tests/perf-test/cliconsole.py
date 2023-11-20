c0e15b85f118eb0de58a66d563987662d82bbfa3
zk66214 <zk662144@163.com>
2023-11-20 14:50:38 +0800

    完善console的命令行校验逻辑

--- tests/perf-test/cliconsole.py
+++ tests/perf-test/cliconsole.py
@@ -7,10 +7,19 @@ import coloredlogs
 import click
 import configparser
 from util.githubutil import GitHubUtil
-from perfpeasant import PerfRunner
+from perfpeasant import Peasant
+from enums.DataScaleEnum import DataScaleEnum
+
 
 # 初始化日志处理
-def initLogger(loggerName: str):
+def initLogger(loggerName: str, log_file: str):
+    # 初始化读取配置文件实例
+    confile = os.path.join(os.path.dirname(__file__), "conf", "config.ini")
+    cf = configparser.ConfigParser()
+    cf.read(confile, encoding='UTF-8')
+
+    perf_log_path = cf.get("machineconfig", "log_path")
+
     # 开启日志记录
     logger = logging.getLogger(loggerName)
 
@@ -20,6 +29,11 @@ def initLogger(loggerName: str):
         "error=background=red,bold;critical=background=red"
     LOG_FORMAT = "%(asctime)s - %(levelname)9s - %(message)s"
     logFormat = logging.Formatter(LOG_FORMAT)
+
+    file_handler = logging.FileHandler(perf_log_path + '/log.txt')
+    file_handler.setFormatter(logFormat)
+    logger.addHandler(file_handler)
+
     consoleLogHandler = logging.StreamHandler()
     consoleLogHandler.setFormatter(logFormat)
 
@@ -58,13 +72,18 @@ def cli(ctx, version):
 # ):
 #     pass
 
-# @cli.command(help="启动一个后台服务，在服务中根据用户输入的分支信息，循环执行性能测试")
-# @click.option("--branches", "-b", type=str, required=True, help="指定分支信息，用逗号分隔.", )
-# @click.option("--data-scale", "-s", type=str, required=False, default="mid", help="性能测试数据规模，暂支持3个级别：big、mid和tiny.", )
+@cli.command(help="启动一个后台服务，在服务中根据用户输入的分支信息，循环执行性能测试")
+@click.option("--branches", "-b", type=str, required=True, help="指定分支信息，用逗号分隔.", )
+@click.option("--data-scale", "-s", type=str, required=False, default="mid", help="性能测试数据规模，暂支持3个级别：big、mid和tiny.", )
+@click.option("--interlace-rows", "-i", type=int, required=False, default=0, help="数据插入模式.默认为0", )
+@click.option("--stt-trigger", "-t", type=int, required=False, default=1, help="触发文件合并的落盘文件的个数.默认为1", )
 def run_PerfTest_Backend(
-        branches,
-        data_scale
+        branches: str,
+        data_scale: str,
+        interlace_rows: int,
+        stt_trigger: int
 ):
+
     # 初始化配置文件读取实例
     confile = os.path.join(os.path.dirname(__file__), "conf", "config.ini")
     cf = configparser.ConfigParser()
@@ -72,32 +91,73 @@ def run_PerfTest_Backend(
 
     # 获取github上对应repo的所有分支
     github_repo = "{0}/{1}".format(cf.get("github", "namespace"), cf.get("github", "project"))
-    github = GitHubUtil(github_repo)
-    # current_branches = github.get_last_commit_by_branch(branch='3.0')
-    current_branches = github.get_branches()
 
+    perf_test_path = cf.get("machineconfig", "perf_test_path")
+    perf_log_path = cf.get("machineconfig", "log_path")
+    benchmark_path = cf.get("machineconfig", "benchmark_path")
+    github_path = cf.get("machineconfig", "github_path")
+    history_path = cf.get("machineconfig", "history_path")
+
+    if os.path.exists(perf_test_path):
+        os.system(f"rm -rf {perf_test_path}")
+    os.system(f"mkdir -p  {benchmark_path}")
+    os.system(f"mkdir -p  {github_path}")
+    os.system(f"mkdir -p  {history_path}")
+    os.system(f"mkdir -p  {perf_log_path}")
+
+    appLogger = initLogger("Performance_testing", perf_log_path + '/log.txt')
+
+    appLogger.info(
+        '性能测试命令：cliconsole.py --branches {0} --data-scale {1} -interlace-rows {2} --stt-trigger {3}'.format(branches,
+                                                                                                            data_scale,
+                                                                                                            interlace_rows,
+                                                                                                            stt_trigger))
     # 解析branch参数
     # 参数校验，若输入参数中有不存在的分支，直接退出
+    github = GitHubUtil(github_repo)
+    current_branches = github.get_branches()
+
     branch_list = branches.split(',')
     for branch in branch_list:
+        if branch == "main":
+            continue
         if branch not in current_branches:
-            print('分支{0}不存在'.format(branch))
+            appLogger.error('输入分支不存在，分支名称：{0}'.format(branch))
             exit(1)
+
+    # 参数校验，若输入参数中有符合的数据规模参数，直接退出
+    perf_test_scale = None
+    if data_scale.lower() == DataScaleEnum.tinyenv.name:
+        perf_test_scale = DataScaleEnum.tinyenv
+    elif data_scale.lower() == DataScaleEnum.midweight.name:
+        perf_test_scale = DataScaleEnum.midweight
+    elif data_scale.lower() == DataScaleEnum.bigweight.name:
+        perf_test_scale = DataScaleEnum.bigweight
+    else:
+        appLogger.error("输入的数据规模格式不对，正确格式：[big、mid、tiny]，实际输入：{0}".format(data_scale))
+        exit(1)
 
     # 循环执行性能测试
     while True:
         # 轮询每个配置的分支，运行一次性能测试
         for branch in branch_list:
             # 初始化性能执行器
-            appLogger = initLogger("Performance_testing")
-            perftestHandler = PerfRunner(logger=appLogger)
+
+            perTester = Peasant(logger=appLogger)
 
             # 配置分支
-            perftestHandler.set_branch(branch=branch)
+            perTester.set_branch(branch=branch)
+
+            # 配置stt_trigger
+            perTester.set_stt_trigger(stt_trigger=stt_trigger)
+
+            # 配置interlace_rows
+            perTester.set_interlace_rows(interlace_rows=interlace_rows)
+
             # 配置数据量级
-            perftestHandler.set_data_scale(scale=data_scale)
+            perTester.set_data_scale(scale=perf_test_scale)
             # 开始跑起来
-            perftestHandler.start()
+            perTester.start()
 
 
 
@@ -115,9 +175,9 @@ def abort_PerfTest_Backend(
 
 if __name__ == "__main__":
 
-    run_PerfTest_Backend("main,3.0,3.1")
-    # 禁用paramiko的一些不必要日志
-    logging.getLogger("paramiko").setLevel(logging.WARNING)
+    # run_PerfTest_Backend("main,3.0,3.1")
+    # # 禁用paramiko的一些不必要日志
+    # logging.getLogger("paramiko").setLevel(logging.WARNING)
 
     # 运行应用程序
     try:
@@ -128,3 +188,5 @@ if __name__ == "__main__":
         print('traceback.format_exc():\n%s' % traceback.format_exc())
         click.secho(repr(ge), err=True, fg="red")
         sys.exit(1)
+
+    pass
