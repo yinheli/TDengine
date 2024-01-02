13c850a8b57d2ecc5dfa6ecec6eeca7e319414e4
dmchen <cademfly@hotmail.com>
2024-01-02 02:06:24 +0000

    str copy

--- source/dnode/vnode/src/vnd/vnodeOpen.c
+++ source/dnode/vnode/src/vnd/vnodeOpen.c
@@ -476,18 +476,17 @@ SVnode *vnodeOpen(const char *path, int32_t diskPrimary, STfs *pTfs, SMsgCb msgC
     vnodeRollback(pVnode);
   }
 
-  snprintf(pVnode->monitor.strClusterId, sizeof(pVnode->monitor.strClusterId), "%" PRId64, 
-          pVnode->config.syncCfg.nodeInfo[0].clusterId);
-  snprintf(pVnode->monitor.strDnodeId, sizeof(pVnode->monitor.strDnodeId), "%"PRId32, 
-            pVnode->config.syncCfg.nodeInfo[0].nodeId);
-  sprintf(pVnode->monitor.strVgId, "%"PRId32, pVnode->config.vgId);
+  snprintf(pVnode->monitor.strClusterId, TSDB_CLUSTER_ID_LEN, "%"PRId64, pVnode->config.syncCfg.nodeInfo[0].clusterId);
+  snprintf(pVnode->monitor.strDnodeId, TSDB_DNODE_ID_LEN, "%"PRId32, pVnode->config.syncCfg.nodeInfo[0].nodeId);
+  snprintf(pVnode->monitor.strVgId, TSDB_VGROUP_ID_LEN, "%"PRId32, pVnode->config.vgId);
 
   if(pVnode->monitor.insertCounter == NULL){
-    int32_t label_count =6;
+    int32_t label_count = 6;
     const char *sample_labels[] = {VNODE_METRIC_TAG_NAME_SQL_TYPE, VNODE_METRIC_TAG_NAME_CLUSTER_ID,
                                    VNODE_METRIC_TAG_NAME_DNODE_ID, VNODE_METRIC_TAG_NAME_DNODE_EP,
                                   VNODE_METRIC_TAG_NAME_VGROUP_ID, VNODE_METRIC_TAG_NAME_USERNAME};
-    taos_counter_t *counter = taos_counter_new(VNODE_METRIC_INSERT_COUNT, "counter for insert sql",  label_count, sample_labels);
+    taos_counter_t *counter = taos_counter_new(VNODE_METRIC_INSERT_COUNT, "counter for insert sql",  
+                                                label_count, sample_labels);
     vInfo("vgId:%d, new metric:%p",TD_VID(pVnode), counter);
     if(taos_collector_registry_register_metric(counter) == 1){
       taos_counter_destroy(counter);
